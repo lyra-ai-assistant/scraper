f3005d63581d682806d5e804b24e9f248ca1d177
Andres M. Prieto <andresprieto2703@gmail.com>
2024-10-07 23:44:52 -0500

    Tracking configuration file for scraper need

---
+++ config/vars.py
@@ -0,0 +1,26 @@
+import json
+from pathlib import Path
+from types import SimpleNamespace
+from os.path import join, dirname
+
+
+project_path = Path(dirname(__file__))
+config_file = join(project_path.parent.absolute(), "config.json")
+
+with open(config_file, 'r') as configFile:
+    config = json.load(configFile)
+
+optional = config.get('optional', {})
+
+vars = SimpleNamespace(
+    scraperName=config.get('scraperName', "spyder"),
+    urlToScrap=config.get('urlToScrap', []),
+    domainToScrap=config.get('domainToScrap', []),
+    outputDirectory=config.get('outputDirectory', ""),
+    verbose=config.get('verbose', False),
+
+    scraperTargets=config.get('selectors', {}),
+
+    outputFileName=optional.get('outputFileName', "crawler_data"),
+    outputFileFormat=optional.get('outputFileFormat', "csv"),
+)
