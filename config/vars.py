5e9d2c44020898ebcd04a8fc42224cb0cb09acf3
Andres M. Prieto <andresprieto2703@gmail.com>
2024-10-11 23:37:25 -0500

    Just linting

--- config/vars.py
+++ config/vars.py
@@ -7,22 +7,18 @@ from os.path import join, dirname
 project_path = Path(dirname(__file__))
 config_file = join(project_path.parent.absolute(), "config.json")
 
-with open(config_file, 'r') as configFile:
+with open(config_file, "r") as configFile:
     config = json.load(configFile)
 
-optional = config.get('optional', {})
+optional = config.get("optional", {})
 
 vars = SimpleNamespace(
-    scraperName=config.get('scraperName', "spider"),
-    urlToScrap=config.get('urlToScrap', []),
-    domainToScrap=config.get('domainToScrap', []),
-
-    outputFileName=optional.get('outputFileName', "crawler_data"),
-    outputFileFormat=optional.get('outputFileFormat', "json"),
-    outputDirectory=optional.get('outputDirectory', "result"),
-
-    verbose=optional.get('verbose', False),
-
-    scraperTargets=config.get('selectors', {}),
-
+    scraperName=config.get("scraperName", "spider"),
+    urlToScrap=config.get("urlToScrap", []),
+    domainToScrap=config.get("domainToScrap", []),
+    outputFileName=optional.get("outputFileName", "crawler_data"),
+    outputFileFormat=optional.get("outputFileFormat", "json"),
+    outputDirectory=optional.get("outputDirectory", "result"),
+    verbose=optional.get("verbose", False),
+    scraperTargets=config.get("selectors", {})
 )
