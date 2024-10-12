bd5e0d006a5e3ac7b708e99b2602641abe43f6c3
Andres M. Prieto <andresprieto2703@gmail.com>
2024-10-11 23:39:25 -0500

    Fixing path for crawler

--- main.py
+++ main.py
@@ -3,21 +3,33 @@ from scrapy.utils.project import get_project_settings
 from lyra_scraper.spiders.simple import SimpleSpider
 
 from config.vars import vars
+from config.dirs import check_path, create_dir
 
 
 def run_spider():
     # Crawler configuration
-    process = CrawlerProcess(settings={
-        "FEED_FORMAT": vars.outputFileFormat,
-        "FEED_URI": vars.outputDirectory + vars.scraperName + "_" + vars.outputFileName + vars.outputFileFormat
-    })
+    process = CrawlerProcess(
+        settings={
+            "FEED_FORMAT": vars.outputFileFormat,
+            "FEED_URI": f"{vars.outputDirectory}/{vars.scraperName}_{vars.outputFileName}.{vars.outputFileFormat}",
+            "LOG_ENABLED": vars.verbose,
+        }
+    )
 
     # Crawls the spider
     process.crawl(SimpleSpider)
 
-    # Bloquear hasta que se complete el spider
+    # Block execution until spider ends
     process.start()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # Validations
+    if not check_path(vars.outputDirectory):
+        create_dir(vars.outputDirectory)
+
+    print(
+        f"Files created at: {vars.outputDirectory}/{vars.scraperName}_{vars.outputFileName}.{vars.outputFileFormat}"
+    )
+
     run_spider()
