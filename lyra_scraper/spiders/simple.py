2ba332699947803e25330399fd41457d4dd3c78f
Andres M. Prieto <andresprieto2703@gmail.com>
2024-10-11 23:30:57 -0500

    Improving CSV files also adding some documentation

--- lyra_scraper/spiders/simple.py
+++ lyra_scraper/spiders/simple.py
@@ -4,9 +4,25 @@ from config.vars import vars
 
 
 class SimpleSpider(scrapy.Spider):
+    """
+    Generic scraper for simple fetching data into specific
+    formats. Its configuration comes from your config.file
+
+    Args (On your config.json):
+        - scraperName (str): Name of the scraper
+        - urlToScrap (array<str>): An array of URLs
+        - domainToScrap (array<str>): An array with the needed domains
+        - scraperTargets (Object): Having the URLs, it's a structure of CSS objects to scrap
+    """
+
     name = vars.scraperName
     start_urls = vars.urlToScrap
     allowed_domains = vars.domainToScrap
+
+    custom_settings = {
+        'csvFields': [key for value in vars.scraperTargets.values() for key in value["fields"].keys()]
+    }
+
 
     def extract_section(self, response, section_selector, fields):
         section_data = response.css(section_selector)
@@ -19,6 +35,7 @@ class SimpleSpider(scrapy.Spider):
     def parse(self, response):
         for section, selectors in vars.scraperTargets.items():
             section_data = self.extract_section(
-                response, selectors['section_selector'], selectors['fields'])
+                response, selectors["section_selector"], selectors["fields"]
+            )
             for item in section_data:
                 yield item
